fba5b556b6dbab11862bf808555b3625f8e6a183
Igor <houwat@gmail.com>
2020-01-18 17:35:26 -0500

    Updates

---
+++ precache-manifest.e5efe5c8ffdd4eb76d4cd4a6778aa18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aff7ff47f82f7c74fa3fa61fcbd0894",
+    "url": "/starwars-api/index.html"
+  },
+  {
+    "revision": "d8766995c9c6516a3296",
+    "url": "/starwars-api/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "15cb0786ec0be581f66e",
+    "url": "/starwars-api/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d8766995c9c6516a3296",
+    "url": "/starwars-api/static/js/2.4fe43bc1.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/starwars-api/static/js/2.4fe43bc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15cb0786ec0be581f66e",
+    "url": "/starwars-api/static/js/main.dd45f12f.chunk.js"
+  },
+  {
+    "revision": "4267af2761763ca662f0",
+    "url": "/starwars-api/static/js/runtime-main.0d3c704e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/starwars-api/precache-manifest.e5efe5c8ffdd4eb76d4cd4a6778aa18d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars-api/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
